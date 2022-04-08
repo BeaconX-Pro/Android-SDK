b88e65e9c54a0ab682634bbd905942f55b775600
liuwenzheng <329541594@qq.com>
2022-04-08 10:05:22 +0800

    fix: Adapter android 12

--- app/src/main/java/com/moko/beaconxpro/activity/MainActivity.java
+++ app/src/main/java/com/moko/beaconxpro/activity/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.elvishew.xlog.XLog;
 import com.moko.beaconxpro.R;
 import com.moko.beaconxpro.dialog.AlertMessageDialog;
+import com.moko.beaconxpro.utils.Utils;
 import com.moko.bxp.nordic.activity.NordicMainActivity;
 import com.moko.bxp.tla.activity.TLAMainActivity;
 
@@ -17,6 +19,19 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_bxp);
+        StringBuffer buffer = new StringBuffer();
+        // 记录机型
+        buffer.append("机型：");
+        buffer.append(android.os.Build.MODEL);
+        buffer.append("=====");
+        // 记录版本号
+        buffer.append("手机系统版本：");
+        buffer.append(android.os.Build.VERSION.RELEASE);
+        buffer.append("=====");
+        // 记录APP版本
+        buffer.append("APP版本：");
+        buffer.append(Utils.getVersionInfo(this));
+        XLog.d(buffer.toString());
     }
 
     public void onSelectBXPNordic(View view) {
